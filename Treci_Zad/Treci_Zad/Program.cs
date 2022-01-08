c7486b0e0dda6a10cb4a23650a60388d36c75cd3
Neven <58472164+neven14@users.noreply.github.com>
2022-01-08 10:02:59 +0100

    Add files via upload

---
+++ Treci_Zad/Treci_Zad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treci_Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz = new int[] { 3, 2, 4, 0, 5, 1, 6, 7, 9, 8 };
+            Sort x = new Sort();
+            x.QuickSort(niz, 1, niz.Length - 2);
+            for (int i = 0; i < niz.Length; i++)
+                Console.WriteLine(niz[i]);
+        }
+    }
+}
---
+++ Treci_Zad/Treci_Zad/Sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treci_Zad
+{
+    class Sort
+    {
+        public void QuickSort(int[] arr, int left, int right)
+        {
+            int lijevo = left, desno = right;
+            int x = arr[(left + right) / 2];
+            do
+            {
+                while (arr[lijevo] < x && lijevo < right)
+                    lijevo++;
+                while (arr[desno] > x && desno > left)
+                    desno--;
+                if (lijevo <= desno)
+                {
+                    int tmp = arr[lijevo];
+                    arr[lijevo] = arr[desno];
+                    arr[desno] = tmp;
+                    lijevo++;
+                    desno--;
+                }
+            }
+            while (lijevo <= desno);
+            if (left < desno)
+                QuickSort(arr, left, desno);
+            if (lijevo < right)
+                QuickSort(arr, lijevo, right);
+        }
+    }
+}
